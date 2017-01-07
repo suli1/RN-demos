34cd68e57d0729c7e8ee442cb83d77d43c57e03e
suli <sss908938889@163.com>
2017-01-07 22:20:53 +0800

    Learn props and state

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -4,15 +4,12 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View, Image} from 'react-native';
 
-export default class AwesomeProject extends Component {
+// 1.Hello World
+
+class AwesomeProject extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -32,11 +29,13 @@ export default class AwesomeProject extends Component {
 
   render() {
     let pic = {
-      uri : 'http://i.dailymail.co.uk/i/pix/2013/01/06/article-2257907-1436A316000005DC-755_634x374.jpg'
+      uri: 'http://i.dailymail.co.uk/i/pix/2013/01/06/article-2257907-1436A316000005DC-755_6' +
+          '34x374.jpg'
     };
-    return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
-    );
+    return (<Image source={pic} style={{
+      width: 193,
+      height: 110
+    }}/>);
   }
 
 }
@@ -46,18 +45,86 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+// AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+// 2.props属性
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{
+        alignSelf: 'center'
+      }}>
+        <Greeting name='Rexxar'/>
+        <Greeting name='Jaina'/>
+        <Greeting name='Vallera'/>
+      </View>
+    );
+  }
+
+}
+
+// AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
+// 3.state状态
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showText: true
+    };
+
+    // 每500毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState({
+        showText: !this.state.showText
+      });
+    }, 500);
+  }
+
+  render() {
+    // 根据当前showText的值决定是否显示text内容
+    let display = this.state.showText
+      ? this.props.text
+      : ' ';
+
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink'/>
+        <Blink text='Yes blinking is so greate'/>
+        <Blink text='Why did they ever take this out of HTML'/>
+        <Blink text='Look at me look at me look at me'/>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
